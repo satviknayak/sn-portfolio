e70715ea821c657eeceab4868c94e6aa6015567f
Satvik S Nayak <nayaksatvik02@gmail.com>
2022-09-24 23:20:40 +0530

    Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='flex absolute top-0 left-0 bg-none h-[4rem] sm:h-[5rem] w-full justify-between px-[1rem] sm:px-[4rem]'>
+    <div className='flex absolute top-0 left-0 bg-none h-[4rem] sm:h-[5rem] w-full justify-between px-[1rem] sm:px-[4rem] z-[99]'>
         <div/>
         <ul className='flex my-auto'>
           <li className='mx-[15px] sm:mx-[1.5rem]'><a href="#About" className='text-slate-400 hover:text-white focus:text-white text-[0.9rem] sm:text-[1.1rem]'>About</a></li>
